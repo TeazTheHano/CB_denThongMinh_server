d88d89c05ba40524c9767945c9c6c8a123959949
Khuong Anh Kiet <teaz.khuonganhkiet@gmail.com>
2024-01-04 17:53:39 +0700

    first look

--- public/script/settingFilter.js
+++ public/script/settingFilter.js
@@ -1,7 +1,26 @@
+// Switch UI
+let switchBtn = document.querySelectorAll('#adminSwitch > div');
+let adminManage = document.querySelector('#adminManage');
+let adminRegister = document.querySelector('#adminRegister');
+switchBtn.forEach(element => {
+    element.addEventListener("click", () => {
+        switchBtn.forEach(btn => {
+            btn.setAttribute("status", btn.getAttribute("status") === "off" ? "on" : "off");
+        });
+
+        if (adminManage.style.display === "none") {
+            adminManage.style.display = "block";
+            adminRegister.style.display = "none";
+        } else {
+            adminManage.style.display = "none";
+            adminRegister.style.display = "block";
+        }
+    });
+});
+
 // Filter UI
 let filterBtn = document.querySelectorAll('.listFilter > div[status]:not([status="shut"])');
 let shutBtn = document.querySelector('.listFilter > div[status="shut"]');
-console.log(filterBtn);
 
 filterBtn.forEach(element => {
     element.addEventListener("click", () => {
